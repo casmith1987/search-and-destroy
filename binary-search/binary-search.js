9714662ab6782fd2831674458470ad81305db1e6
LuFrei <lucasfreitas09876@gmail.com>
2020-10-28 15:35:17 -0400

    Binary Search Array done. Extra Credit not done

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,8 +2,36 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
+	//find mid point
+	while(array.length){
+		let midPoint = Math.floor(array.length / 2)
+
+		// midepoint/2 if right right side: (+ midpoint)
+
+		//compare to target
+		if(array[midPoint] === target){
+			return true;
+		} else if (target < array[midPoint]){
+			array = array.slice(0, midPoint)
+		} else if (target > array[midPoint]){
+			array = array.slice(midPoint+1)
+		}
+		console.log(array)
+	}
+	return false
 };
+
+//Rules:
+//1. Array will be sorted
+//2. should run in O(log(n))
+
+//Method:
+//1. Find mid point of array
+//2. Check if target is > || < || = midpoint
+//3. get rid (array.slice()) of unapplicable half.
+//4. keep going until midpoint = target
+// or we run out array elements (does not exist in array)
+
 
 /*
 	EXTRA CREDIT:
